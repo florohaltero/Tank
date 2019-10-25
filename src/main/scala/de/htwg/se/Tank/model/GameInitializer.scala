8d565219a4e91865591245ed1d167ef4543c7158
florohaltero <Flo_179324865>
2019-10-25 12:36:27 +0200

    Specs

---
+++ src/main/scala/de/htwg/se/Tank/model/GameInitializer.scala
@@ -0,0 +1,12 @@
+package de.htwg.se.Tank.model
+
+object GameInitializer {
+  val version = 0.1
+  val name = "Tank"
+  var partyname = "Battle"
+
+  def setPartyName(s: String): String = {
+    partyname = s
+    partyname
+  }
+}
--- src/test/scala/de/htwg/se/Tank/model/PositionSpec.scala
+++ src/test/scala/de/htwg/se/Tank/model/PositionSpec.scala
@@ -1,5 +1,18 @@
 package de.htwg.se.Tank.model
+import org.scalatest._
 
-class PositionSpec {
+
+class PositionSpec extends WordSpec with Matchers {
+  "A Position" when {
+    "new" should {
+      var pos = new Position(100, 100)
+      "have x " in {
+        pos.x should be (100)
+      }
+      " have y" in {
+        pos.y should be (100)
+      }
+    }
+  }
 
 }
