4f4f21cb9176892c8939679ef9a82fb51144525f
Sascha <sascha.ivan99@gmail.com>
2019-11-07 15:53:53 +0100

    push all

--- src/main/scala/de/htwg/se/Tank/Tank.scala
+++ src/main/scala/de/htwg/se/Tank/Tank.scala
@@ -9,13 +9,14 @@ object Tank {
   def main(args: Array[String]): Unit = {
     //val student = Player("Your Name")
     //println("Hello, " + student.name)
-
+    val lines = scala.io.Source.fromFile("Tank_label.txt").mkString
+    print(lines)
     var input : String = ""
     val tui = new TUI
-    var g1: Game = GameInitializer.setGame("Standard", 1, "Flo", "Sascha")
+    var g1: Game = GameInitializer.setGame("Standard", 0, "Flo", "Sascha")
     //println("Type name of Player1 and Player2:")
     do {
-      println("Game : " + g1.toString)
+      println(g1.toString)
       input = scala.io.StdIn.readLine()
       g1 = tui.processInputLine(input, g1)
     } while (input != "q")
