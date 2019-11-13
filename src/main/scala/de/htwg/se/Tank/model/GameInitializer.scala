e019e2bed3e1f3d07a1a7f163ea2a1156ac6e792
florohaltero <56345912+florohaltero@users.noreply.github.com>
2019-11-13 18:12:06 +0100

    Erweiterung um automatischen Player Wechsel und Randüberlauf

--- src/main/scala/de/htwg/se/Tank/model/GameInitializer.scala
+++ src/main/scala/de/htwg/se/Tank/model/GameInitializer.scala
@@ -7,14 +7,6 @@ object GameInitializer {
   def setGame(partyname :String, map: Int, name1: String, name2: String): Game ={
     Game(partyname, map , name1, name2)
   }
-  def  moveLeft(game: Game, p: Player) : Game = {
-    game.mapObject.moveLeft(p)
-    game
-  }
-  def  moveRight(game: Game, p: Player) : Game = {
-    game.mapObject.moveRight(p)
-    game
-  }
 
 
 }
