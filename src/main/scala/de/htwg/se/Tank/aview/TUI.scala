b40808286c070886b05d21d5bebfc8c264143c07
Sascha <sascha.ivan99@gmail.com>
2019-11-20 13:29:16 +0100

    Continuous Deployment

--- src/main/scala/de/htwg/se/Tank/aview/TUI.scala
+++ src/main/scala/de/htwg/se/Tank/aview/TUI.scala
@@ -11,8 +11,7 @@ class TUI(controller: Controller) extends Observer{
     input match {
       case "m0" => controller.setGame("Standard", 0, controller.game.player1, controller.game.player2)
       case "m1" => controller.setGame("Standard", 1, controller.game.player1, controller.game.player2)
-      case x => if (x == "p") controller.setGame("Standard", 1, "Player1","Player2")
-                  else println("Game konnte nicht erstellt werden")
+      case "p" => controller.setGame("Standard", 1, "Player1","Player2")
       case x => if (x == "l") controller.moveLeft() else println("Fehler Eingabe")
       case x => if (x == "r") controller.moveRight() else println("Fehler Eingabe")
     }
