e8cafe782f193a40d075fb9b558c91410aabefa7
florohaltero <56345912+florohaltero@users.noreply.github.com>
2019-11-28 16:42:03 +0100

    Factory Method Pattern in Player inserted

--- src/test/scala/de/htwg/se/Tank/model/TankSpec.scala
+++ src/test/scala/de/htwg/se/Tank/model/TankSpec.scala
@@ -7,8 +7,13 @@ import org.scalatest.junit.JUnitRunner
 class TankSpec extends WordSpec with Matchers {
   val pos = Position(200,50)
   val angle = 10
+  val step = 5
   "A Tank" when { "new" should {
     var tank = Tank(pos, 100, angle)
+    "have vars" in {
+      tank.step should be (step)
+      tank.pos should be (pos)
+    }
     "have Position X" in {
       tank.pos.x should be (200)
     }
