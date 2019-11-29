ddc01962faf70071c5fe9443450f8c9e5198ae4f
Sascha <sascha.ivan99@gmail.com>
2019-11-29 12:58:20 +0100

    More Tests

--- src/main/scala/de/htwg/se/Tank/model/Player.scala
+++ src/main/scala/de/htwg/se/Tank/model/Player.scala
@@ -1,15 +1,20 @@
 package de.htwg.se.Tank.model
+
+import scala.collection.mutable.ListBuffer
+
 trait Player{
    val id : Integer
    var tank : Tank
    val name : String
    var pos : Position
+   var lp: Int
+   var ItemList: ListBuffer[ItemTemplate] = ListBuffer.empty
 }
 
 object PlayerFactory {
    val PLAYER1: Int = 1
    val PLAYER2: Int = 2
-   val LIFEPOINTS: Int = 100
+   val LP: Int = 100
    val DEFAULT_A: Int = 30
 
    def createPlayer1(name: String, pos: Position): Player = {
@@ -27,7 +32,8 @@ object PlayerFactory {
          val s = "Player: " + name + "\n" + tank.toString
          s
       }
-      var tank = Tank(pos, LIFEPOINTS, DEFAULT_A)
+      var tank = Tank(pos, LP, DEFAULT_A)
+      override var lp: Int = LP
    }
 
    case class Player2(name: String, var pos: Position) extends Player {
@@ -36,7 +42,8 @@ object PlayerFactory {
          val s = "Player: " + name + "\n" + tank.toString
          s
       }
-      var tank = Tank(pos, LIFEPOINTS, DEFAULT_A)
+      var tank = Tank(pos, LP, DEFAULT_A)
+      override var lp: Int = LP
    }
 
 }
