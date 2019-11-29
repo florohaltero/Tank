d4d2d9881e0fbd7850f87f03b009203a399dc143
florohaltero <Flo_179324865>
2019-11-29 15:24:06 +0100

    Template Pattern

--- src/main/scala/de/htwg/se/Tank/model/ItemTemplate.scala
+++ src/main/scala/de/htwg/se/Tank/model/ItemTemplate.scala
@@ -2,30 +2,48 @@ package de.htwg.se.Tank.model
 
 trait ItemTemplate {
   val name :  String
-  def collectItem(player: Player): Unit = {
-    player.ItemList.append(this)
+  def activateItem(player: Player): Unit = {
+    if(!isItemAlreadyAvailable(player)) {
+      collect(player)
+      println(msgItem())
+    } else {
+      println(msgItemUsed())
+      return
+    }
+    useItem(player)
   }
   def useItem(player: Player): Unit
-
+  def msgItem() : String
   val value: Integer
+  def msgItemUsed() : String = {
+    "Item already used"
+  }
+  def collect(player: Player): Unit ={
+    player.ItemList.append(this)
+  }
+  def isItemAlreadyAvailable(player: Player): Boolean = {
+    player.ItemList.contains(this)
+  }
 }
 
 case class ItemHealth() extends ItemTemplate {
   override val name: String = "Health"
-
   override def useItem(player: Player): Unit = {
     player.lp + value
-    println("Name: " + player.name + "Life: " + player.lp)
   }
   override val value: Integer = 25
+  override def msgItem(): String = {
+    "Healt Item"
+  }
 }
 
 case class ItemMoreMoves() extends ItemTemplate {
   override val name: String = "More Moves"
-
   override def useItem(player: Player): Unit = {
-    println("Moves + 1")
+    //use more moves
   }
-
   override val value: Integer = 2
+  override def msgItem(): String = {
+    "More Moves Item"
+  }
 }
