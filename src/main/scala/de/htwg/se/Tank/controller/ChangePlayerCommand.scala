394cffc1f89b065f1826ee48a9c206da0d22b71b
florohaltero <56345912+florohaltero@users.noreply.github.com>
2019-12-05 20:10:44 +0100

    Command Pattern erfolgreich hinzugefügt

--- src/main/scala/de/htwg/se/Tank/controller/ChangePlayerCommand.scala
+++ src/main/scala/de/htwg/se/Tank/controller/ChangePlayerCommand.scala
@@ -4,21 +4,23 @@ import de.htwg.se.Tank.model.Map
 import de.htwg.se.Tank.util.Command
 
 class ChangePlayerCommand(controller: Controller) extends Command{
-var memento : Map = controller.game.mapObject
+  var memento: (Integer) = _
   override def doStep: Unit = {
-    memento = controller.game.mapObject
+    memento = controller.game.mapObject.moves
     controller.game.mapObject.StateContext.state.changePlayer()
   }
 
   override def undoStep: Unit = {
-    val new_memento = controller.game.mapObject
-    controller.game.mapObject = memento
+    val new_memento = controller.game.mapObject.moves
+    controller.game.mapObject.StateContext.state.changePlayer()
+    controller.game.mapObject.moves = memento
     memento = new_memento
   }
 
   override def redoStep: Unit = {
-    val new_memento = controller.game.mapObject
-    controller.game.mapObject = memento
+    val new_memento = controller.game.mapObject.moves
+    controller.game.mapObject.StateContext.state.changePlayer()
+    controller.game.mapObject.moves = memento
     memento = new_memento
   }
 }
