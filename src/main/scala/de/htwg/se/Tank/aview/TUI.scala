113c188e1f87ad68f6e314eeee44548f2ac220a0
Sascha <sascha.ivan99@gmail.com>
2019-12-05 20:25:10 +0100

    Command Pattern

--- src/main/scala/de/htwg/se/Tank/aview/TUI.scala
+++ src/main/scala/de/htwg/se/Tank/aview/TUI.scala
@@ -4,11 +4,25 @@ import de.htwg.se.Tank.controller.{Controller, GameStatus}
 import de.htwg.se.Tank.model.Game
 import de.htwg.se.Tank.util.Observer
 
+import scala.util.{Failure, Success, Try}
+
 class TUI(controller: Controller) extends Observer{
   controller.add(this)
 
   def processInputLine(input: String): Unit ={
-    input match {
+//    input match {
+//      case "m0" => controller.setGame("Standard", 0, controller.game.name1, controller.game.name2)
+//      case "m1" => controller.setGame("Standard", 1, controller.game.name1, controller.game.name2)
+//      case "a" => controller.moveLeft()
+//      case "d" => controller.moveRight()
+//      case "w" => controller.moveAngleUp()
+//      case "s" => controller.moveAngleDown()
+//      case "p" => controller.changePlayer()
+//      case "f" => controller.shoot()
+//      case "z" => controller.undo
+//      case "y" => controller.redo
+//    }
+    Try(input match {
       case "m0" => controller.setGame("Standard", 0, controller.game.name1, controller.game.name2)
       case "m1" => controller.setGame("Standard", 1, controller.game.name1, controller.game.name2)
       case "a" => controller.moveLeft()
@@ -19,8 +33,13 @@ class TUI(controller: Controller) extends Observer{
       case "f" => controller.shoot()
       case "z" => controller.undo
       case "y" => controller.redo
+      }
+    )  match {
+      case Success(e) =>
+      case Failure(e) => println("falsche Eingabe")
     }
-  }
+    }
+
 
   override def update: Boolean = {
     println(controller.gametoString);
