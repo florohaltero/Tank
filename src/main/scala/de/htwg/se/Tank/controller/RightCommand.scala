2abe20ebb0e7dce4e862de59f0083d43c0fe1979
florohaltero <Flo_179324865>
2019-12-05 20:10:44 +0100

    Command Pattern erfolgreich hinzugefügt

--- src/main/scala/de/htwg/se/Tank/controller/RightCommand.scala
+++ src/main/scala/de/htwg/se/Tank/controller/RightCommand.scala
@@ -3,9 +3,15 @@ package de.htwg.se.Tank.controller
 import de.htwg.se.Tank.util.Command
 
 class RightCommand(controller: Controller) extends Command {
-  override def doStep: Unit = controller.game.mapObject.activePlayer = controller.game.mapObject.moveRight(false)
+  override def doStep: Unit = {
+    controller.game.mapObject.activePlayer = controller.game.mapObject.moveRight()
+  }
 
-  override def undoStep: Unit = controller.game.mapObject.activePlayer = controller.game.mapObject.moveLeft(true)
+  override def undoStep: Unit = {
+    controller.game.mapObject.activePlayer = controller.game.mapObject.undoMoveRight()
+  }
 
-  override def redoStep: Unit = controller.game.mapObject.activePlayer = controller.game.mapObject.moveRight(false)
+  override def redoStep: Unit = {
+    controller.game.mapObject.activePlayer = controller.game.mapObject.moveRight()
+  }
 }
