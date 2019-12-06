5c262e337a0600700ae983b031f42f6143899f38
florohaltero <56345912+florohaltero@users.noreply.github.com>
2019-12-06 18:16:42 +0100

    Erfolgreich auf Map Object umgebaut

--- src/main/scala/de/htwg/se/Tank/model/GameInit.scala
+++ src/main/scala/de/htwg/se/Tank/model/GameInit.scala
@@ -10,12 +10,12 @@ object GameInit {
   final val MAP_Y1 = 0
 
   def setMapSettings(mapNum: Int, name1: String,name2: String) : Unit = {
+    Map.setFX(Option(mapNum))
+    Map.ListFX = Map.getFXList()
     Map.p1 = PlayerFactory.createPlayer1(name1)
     Map.p2 = PlayerFactory.createPlayer2(name2)
-    Map.setFX(Option(mapNum))
     Map.beginOfMap = (MAP_X1,MAP_Y1)
     Map.endOfMap = (MAP_X2,MAP_Y2)
-    Map.ListFX = Map.getFXList()
     Map.StateContext.setState(StateContext.P1State())
   }
 }
