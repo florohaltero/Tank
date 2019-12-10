c248151f6fcf3850dc7722285cab6ae56ba552e8
Sascha <sascha.ivan99@gmail.com>
2019-12-10 22:05:17 +0100

    Command Pattern

--- src/main/scala/de/htwg/se/Tank/aview/gui/FxGui.scala
+++ src/main/scala/de/htwg/se/Tank/aview/gui/FxGui.scala
@@ -11,10 +11,11 @@ import scalafx.scene.paint.{LinearGradient, Stops}
 import scalafx.scene.text.Text
 import scalafx.Includes._
 import scalafx.event.ActionEvent
-import scalafx.scene.control.{Button, CheckBox, Label}
+import scalafx.scene.control.{Alert, Button, CheckBox, Label}
 import scalafx.scene.shape.{Circle, Polyline, Rectangle}
 import de.htwg.se.Tank.controller.Controller
 import de.htwg.se.Tank.model.{Game, GameInit, Map}
+import scalafx.scene.control.Alert.AlertType
 
 import scala.util.Random
 
@@ -26,34 +27,43 @@ object ScalaFXHelloWorld extends JFXApp {
     height = 600
     scene = new Scene {
       fill = LightBlue
-      val box = new HBox {
-        padding = Insets(20)
-        alignmentInParent = Pos.BaselineCenter
-        children = new Text {
-          text = "Tank"
-          style = "-fx-font-size: 100pt"
-          fill = new LinearGradient(
-            stops = Stops(Black, SaddleBrown)
-          )
-          effect = new DropShadow {
-            color = SaddleBrown
-            radius = 50
-            spread = 0.35
-          }
+      val text = new Text {
+        text = "Tank"
+        style = "-fx-font-size: 100pt"
+        fill = new LinearGradient(
+          stops = Stops(Black, SaddleBrown)
+        )
+        effect = new DropShadow {
+          color = SaddleBrown
+          radius = 50
+          spread = 0.35
         }
-
       }
-      box.setAlignment(Pos.Center)
+      text.layoutX = 235
+      text.layoutY = 200
+
+
       val start = new Button("Start Game")
+      start.scaleX = 2
+      start.scaleY = 2
       start.layoutX = 350
       start.layoutY = 300
       val end = new Button("End Game")
+      end.scaleX = 2
+      end.scaleY = 2
       end.layoutX = 350
       end.layoutY = 400
       start.onAction = (e:ActionEvent) => {
         createMapGUI()
       }
-      content = List(start, end, box)
+      end.onAction = (e:ActionEvent) => {
+        val alert = new Alert(AlertType.Confirmation)
+        alert.setTitle("")
+        alert.setHeaderText("Wirklich Verlassen?")
+        alert.showAndWait()
+        stage.close()
+      }
+      content = List(start, end, text)
     }
   }
 
