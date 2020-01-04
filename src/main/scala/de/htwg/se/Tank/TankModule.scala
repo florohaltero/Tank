705261b163e16b56594513839d8d34feb26ad6fd
Sascha <sascha.ivan99@gmail.com>
2020-01-04 17:39:41 +0100

    TankModule erstellt und Main angepasst

---
+++ src/main/scala/de/htwg/se/Tank/TankModule.scala
@@ -0,0 +1,8 @@
+package de.htwg.se.Tank
+
+import com.google.inject.AbstractModule
+import net.codingwell.scalaguice.ScalaModule
+
+class TankModule extends AbstractModule with ScalaModule {
+  override def configure(): Unit = ???
+}
