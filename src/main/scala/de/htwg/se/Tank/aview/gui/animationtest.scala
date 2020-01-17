e1e7561472a0a432afa5d66ecd17ed1f4270890c
florohaltero <Flo_179324865>
2020-01-17 11:33:07 +0100

    Sauberer

--- src/main/scala/de/htwg/se/Tank/aview/gui/animationtest.scala
+++ src/main/scala/de/htwg/se/Tank/aview/gui/animationtest.scala
@@ -1,36 +1,35 @@
 package de.htwg.se.Tank.aview.gui
 
-import scalafx.animation.PathTransition
+import scalafx.animation.{Animation, PathTransition}
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.Scene
+import scalafx.scene.image.{Image, ImageView}
+import scalafx.scene.layout.{BorderPane, HBox}
 import scalafx.scene.paint.Color.Black
 import scalafx.scene.shape.{Circle, Rectangle}
 import scalafx.util.Duration
 
 import scala.swing.{MainFrame, Panel}
 
-object animationtest {
+object animationtest extends JFXApp {
 
-  def main(args: Array[String]): Unit = {
-    val frame = new MainFrame(){
+  def show() : Unit = {
+    stage = new PrimaryStage {
+      scene = new Scene(1000,1000){
+        val image = new Image("de/htwg/se/Tank/aview/gui/Game-Tank-Background-Free-Picture.jpg")
+        val explosion = new ImageView(image)
+        content = explosion
 
+      }
     }
-
-    val panel = new Panel {
-      val rect = new Rectangle()
-      rect.width = 200
-      rect.height = 200
-      rect.x = 30
-      rect.y = 30
-      rect.fill = Black
-      val mun = Circle(200,Black)
-      val fire = new PathTransition(new Duration(100000),rect)
-      //fire.node = mun
-      fire.setNode(mun)
-      fire.setRate(200)
-      fire.setPath(rect)
-      fire.play()
-    }
-    frame.contents = panel
-    frame.open()
   }
 
+
+}
+
+object run {
+  def main(args: Array[String]): Unit = {
+    animationtest.show
+  }
 }
