a4c95fecb421ac9e0bd0ab49ee012b61fd5c07b0
Sascha <sascha.ivan99@gmail.com>
2020-01-21 16:56:45 +0100

    Tests

--- src/test/scala/de/htwg/se/Tank/model/gameComponentSpec/gameBaseSpec/CalcSpec.scala
+++ src/test/scala/de/htwg/se/Tank/model/gameComponentSpec/gameBaseSpec/CalcSpec.scala
@@ -1,7 +1,8 @@
 package de.htwg.se.Tank.model.gameComponentSpec.gameBaseSpec
 
 import de.htwg.se.Tank.controller.controllerComponent.controllerBaseImpl.controller.Controller
-import de.htwg.se.Tank.model.gameComponent.gameBase.{Calc, Game}
+import de.htwg.se.Tank.model.gameComponent.gameBase.{Calc, Game, Map}
+import de.htwg.se.Tank.model.playerComponent.playerBase.Player
 import org.junit.runner.RunWith
 import org.scalatest._
 import org.scalatest.junit.JUnitRunner
@@ -13,13 +14,20 @@ class CalcSpec extends WordSpec with Matchers {
   val controller = new Controller(game)
   "Calculation" when {
     "shootCalc" should {
-      var obj = Calc
+      val obj = Calc
       "G" in {
         obj.G should be(9.81)
       }
       "shootCalc List" in {
         val l = Calc.shootCalc(true)
         l should not be(null)
+      }
+     val player: Player = Map.p2
+      "when hit" in {
+        obj.hit(player.pos.x + player.pos.x, 0) should be (true)
+      }
+      "when win" in {
+        obj.win(player) should be (true)
       }
     }
   }
